b5a09e0b65367e2a7f09552df02de42f69e13b2d
Kelvin Steiner <kelvinsteinersantos@gmail.com>
2016-09-22 22:46:17 -0300

    update setup.py

--- setup.py
+++ setup.py
@@ -4,11 +4,11 @@ from distutils.core import setup
 
 setup(
     name='unicode-text-style',
-    version='0.1',
-    description='Python Unicode combining characters styler',
+    version='0.2',
+    description='Script that styles a piece of text using combining Unicode characters',
     author='Kelvin Santos',
     author_email='kelvinsteinersantos@gmail.com',
-    url='https://github.com/kelvinss',
+    url='https://github.com/kelvinss/unicode-text-style',
     py_modules = ['unicode_text_style'],
     scripts = ['text-style'],
     extras_require = {
